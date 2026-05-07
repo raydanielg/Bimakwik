b03eaddb077dc7ed60de848753ae49d2c460044d
raydanielg <airezra2+1@gmail.com>
2026-05-07 23:50:17 +0300

    commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,29 @@ mix.options({
 
 process.env.NODE_ENV = 'test';
 
+mix.webpackConfig((webpack, config) => {
+    if (!config || !config.module || !Array.isArray(config.module.rules)) {
+        return {};
+    }
+
+    for (const rule of config.module.rules) {
+        const ruleUses = rule && rule.use;
+        const uses = Array.isArray(ruleUses) ? ruleUses : [];
+
+        for (const use of uses) {
+            if (!use || typeof use !== 'object') continue;
+
+            if (typeof use.loader === 'string' && use.loader.includes('sass-loader')) {
+                use.options = use.options || {};
+                use.options.sassOptions = use.options.sassOptions || {};
+                use.options.sassOptions.quietDeps = true;
+            }
+        }
+    }
+
+    return {};
+});
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
