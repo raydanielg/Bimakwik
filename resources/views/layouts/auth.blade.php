a48bb4a907104a7894ab7684ca6c6f3daebb0b70
raydanielg <airezra2+1@gmail.com>
2026-05-08 00:10:18 +0300

    commit

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,6 +9,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
 <body>
     <div id="app">
