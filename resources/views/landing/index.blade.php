23f230503a486f1ed998977483a27cf1b88593b5
raydanielg <airezra2+1@gmail.com>
2026-05-08 00:30:01 +0300

    commit

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -252,6 +252,28 @@
                 crossFade: true
             },
         });
+
+        const productsSwiper = new Swiper('.products-slider', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+            },
+        });
     });
 </script>
 @endsection
