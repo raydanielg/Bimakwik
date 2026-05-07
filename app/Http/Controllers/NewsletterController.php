bd6478705f15ae23663c48aafd7f88b7c5ec477f
raydanielg <airezra2+1@gmail.com>
2026-05-08 00:39:01 +0300

    commit

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Newsletter;
 use Illuminate\Http\Request;
 
 class NewsletterController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|unique:newsletters,email',
+        ], [
+            'email.unique' => 'This email is already subscribed!',
+        ]);
+
+        Newsletter::create([
+            'email' => $request->email,
+        ]);
+
+        return back()->with('success', 'Thank you for subscribing to our newsletter!');
+    }
 }
