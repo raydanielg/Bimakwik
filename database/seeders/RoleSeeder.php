9f6ea2aed9f1ac8bc76ddbcb94f2db4c4bb5f0fd
raydanielg <airezra2+1@gmail.com>
2026-05-08 15:19:35 +0300

    commit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -13,6 +13,23 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $roles = [
+            ['name' => 'Super Administrator', 'slug' => 'super-admin', 'description' => 'System wide access'],
+            ['name' => 'Sub-Administrator', 'slug' => 'sub-admin', 'description' => 'Limited administrative access'],
+            ['name' => 'Insurance Companies (Insurers)', 'slug' => 'insurer', 'description' => 'Manage policies and claims'],
+            ['name' => 'Brokers / Aggregators', 'slug' => 'broker', 'description' => 'Intermediary between insurers and agents'],
+            ['name' => 'Insurance Agents', 'slug' => 'agent', 'description' => 'Direct sales to customers'],
+            ['name' => 'Sales Force Executives (SFEs)', 'slug' => 'sfe', 'description' => 'Field sales team'],
+            ['name' => 'Bancassurance Agents', 'slug' => 'bancassurance', 'description' => 'Bank-based insurance sales'],
+            ['name' => 'Customers', 'slug' => 'customer', 'description' => 'Policy holders and app users'],
+            ['name' => 'Service Providers', 'slug' => 'service-provider', 'description' => 'Hospitals, Pharmacies, Garages, etc.'],
+            ['name' => 'Regulator', 'slug' => 'regulator', 'description' => 'TIRA and other regulatory bodies'],
+            ['name' => 'Premium Financing Partners', 'slug' => 'financing-partner', 'description' => 'Partners providing premium loans'],
+            ['name' => 'Developers / API Integrators', 'slug' => 'developer', 'description' => 'External technical partners'],
+        ];
+
+        foreach ($roles as $role) {
+            \App\Models\Role::create($role);
+        }
     }
 }
