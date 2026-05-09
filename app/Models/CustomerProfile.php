5c5e651449c613844b9fa1181349524ff29aa249
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:05:26 +0300

    commit

--- app/Models/CustomerProfile.php
+++ app/Models/CustomerProfile.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class CustomerProfile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date_of_birth',
+        'gender',
+        'nationality',
+        'residential_address',
+        'postal_address',
+        'city',
+        'ward',
+        'district',
+        'region',
+        'occupation',
+        'employer_name',
+        'emergency_contact_name',
+        'emergency_contact_phone',
+        'profile_photo_url'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
