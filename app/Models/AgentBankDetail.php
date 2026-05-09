0f3ae4dc424a9241996d0b4ddcb0848efd388059
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:41:46 +0300

    commit

---
+++ app/Models/AgentBankDetail.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AgentBankDetail extends Model
+{
+    use HasFactory;
+}
