d9aa5563fc40b90c873fa4e2b09264805cc8f3ba
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:50:14 +0300

    commit

---
+++ app/Models/ApiRateLimit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ApiRateLimit extends Model
+{
+    use HasFactory;
+}
