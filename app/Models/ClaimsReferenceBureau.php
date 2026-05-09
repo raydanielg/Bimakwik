a75a5b9d523069d0436f32c4c8cadbf27eba581d
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:47:26 +0300

    commit

---
+++ app/Models/ClaimsReferenceBureau.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClaimsReferenceBureau extends Model
+{
+    use HasFactory;
+}
