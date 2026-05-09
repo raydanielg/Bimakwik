5a0d6ce3afe04d878133e02b67650602ffc52959
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:32:34 +0300

    commit

--- database/migrations/2026_05_09_203053_create_policy_document_access_logs_table.php
+++ database/migrations/2026_05_09_203053_create_policy_document_access_logs_table.php
@@ -15,7 +15,15 @@ class CreatePolicyDocumentAccessLogsTable extends Migration
     {
         Schema::create('policy_document_access_logs', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('policy_document_id');
+            $table->unsignedBigInteger('user_id');
+            $table->enum('access_type', ['view', 'download', 'share', 'print']);
+            $table->ipAddress('ip_address');
+            $table->text('user_agent')->nullable();
             $table->timestamps();
+
+            $table->foreign('policy_document_id')->references('id')->on('policy_documents')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
