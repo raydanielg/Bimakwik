720bd69c89536334416867765a6cbdc4331c6943
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:39:18 +0300

    commit

---
+++ app/Models/DashboardSummary.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DashboardSummary extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2026_05_09_203904_create_policy_cancellations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePolicyCancellationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('policy_cancellations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('policy_cancellations');
+    }
+}
