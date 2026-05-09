f611f8a289aa80ff9dcc5cbe7fb3386b916c0b72
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:37:53 +0300

    commit

---
+++ app/Models/HospitalList.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class HospitalList extends Model
+{
+    use HasFactory;
+}
