e8549e98917a4bdcd92f7b3c885ef0c6744cd838
raydanielg <airezra2+1@gmail.com>
2026-05-09 23:41:25 +0300

    commit

---
+++ database/migrations/2026_05_09_204119_create_aggregator_agreements_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAggregatorAgreementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aggregator_agreements', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aggregator_agreements');
+    }
+}
