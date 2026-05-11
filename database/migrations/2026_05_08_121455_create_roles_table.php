5deb67ede8093a70cac5cf252458e9dfaff1b8db
raydanielg <airezra2+1@gmail.com>
2026-05-11 09:13:22 +0300

    commit

--- database/migrations/2026_05_08_121455_create_roles_table.php
+++ database/migrations/2026_05_08_121455_create_roles_table.php
@@ -15,9 +15,10 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('slug')->unique();
+            $table->string('name')->unique();
+            $table->string('display_name');
             $table->text('description')->nullable();
+            $table->boolean('is_default')->default(false);
             $table->timestamps();
         });
     }
