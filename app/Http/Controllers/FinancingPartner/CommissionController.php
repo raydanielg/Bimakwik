b7a6e64c939f6d995a762e522f6d2b3b220b4466
raydanielg <airezra2+1@gmail.com>
2026-05-11 12:41:16 +0300

    commit

---
+++ app/Http/Controllers/FinancingPartner/CommissionController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\FinancingPartner;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommissionController extends Controller
+{
+    //
+}
