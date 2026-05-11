4f8a12fe504da873582e64131c3ca4f309819395
raydanielg <airezra2+1@gmail.com>
2026-05-11 12:09:02 +0300

    commit

---
+++ app/Http/Controllers/Bancassurance/ComplianceController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Bancassurance;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ComplianceController extends Controller
+{
+    //
+}
