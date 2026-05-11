f6c1ac1fbfa86633aa4b1d23cab2c1233aeb1dbe
raydanielg <airezra2+1@gmail.com>
2026-05-11 17:19:24 +0300

    commit

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Module;
+use App\Models\Role;
 
 class Permission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'module_id',
+        'permission_code',
+        'permission_name',
+        'permission_type',
+        'name',
+        'slug',
+    ];
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
