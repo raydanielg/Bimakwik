8b26f0688539b38ff332dfb2af52b3a2d863681d
raydanielg <airezra2+1@gmail.com>
2026-05-27 20:01:04 +0300

    commit

--- app/Models/FinancingDefault.php
+++ app/Models/FinancingDefault.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class FinancingDefault extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'financing_loan_id', 'default_date', 'default_reason', 'outstanding_amount',
+        'overdue_days', 'collection_attempts', 'written_off', 'written_off_at', 'notes',
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(FinancingLoan::class, 'financing_loan_id');
+    }
 }
