e9a5c864b2477bde67e445b8af483ad956cdbe8d
raydanielg <airezra2+1@gmail.com>
2026-05-27 20:02:02 +0300

    commit

--- app/Models/DeveloperApiKey.php
+++ app/Models/DeveloperApiKey.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class DeveloperApiKey extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'developer_app_id', 'api_key', 'api_secret_hash', 'key_name',
+        'permissions', 'allowed_ips', 'rate_limit_per_minute',
+        'expires_at', 'last_used_at', 'is_active',
+    ];
+
+    protected $casts = [
+        'permissions' => 'array',
+        'allowed_ips' => 'array',
+        'expires_at' => 'datetime',
+        'last_used_at' => 'datetime',
+    ];
+
+    public function developerApp()
+    {
+        return $this->belongsTo(DeveloperApp::class, 'developer_app_id');
+    }
 }
--- app/Models/DeveloperApp.php
+++ app/Models/DeveloperApp.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class DeveloperApp extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id', 'app_name', 'app_description', 'app_logo_url',
+        'app_website', 'redirect_uris', 'is_active',
+    ];
+
+    protected $casts = ['redirect_uris' => 'array'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function apiKeys()
+    {
+        return $this->hasMany(DeveloperApiKey::class, 'developer_app_id');
+    }
 }
