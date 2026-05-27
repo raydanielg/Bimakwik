86187dbfad67939c9c12705ffbdf1204f9d6133f
raydanielg <airezra2+1@gmail.com>
2026-05-27 19:59:47 +0300

    commit

--- app/Models/FinancingLoan.php
+++ app/Models/FinancingLoan.php
@@ -8,4 +8,41 @@ use Illuminate\Database\Eloquent\Model;
 class FinancingLoan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'loan_number', 'customer_id', 'customer_policy_id', 'financing_partner_id',
+        'principal_amount', 'interest_rate', 'total_interest', 'total_repayment',
+        'term_months', 'monthly_installment', 'status', 'approved_by',
+        'approved_at', 'disbursed_at', 'completed_at', 'defaulted_at',
+    ];
+
+    public function financingPartner()
+    {
+        return $this->belongsTo(FinancingPartner::class, 'financing_partner_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function customerPolicy()
+    {
+        return $this->belongsTo(CustomerPolicy::class);
+    }
+
+    public function disbursements()
+    {
+        return $this->hasMany(FinancingDisbursement::class, 'financing_loan_id');
+    }
+
+    public function repaymentSchedules()
+    {
+        return $this->hasMany(FinancingRepaymentSchedule::class, 'financing_loan_id');
+    }
+
+    public function defaults()
+    {
+        return $this->hasMany(FinancingDefault::class, 'financing_loan_id');
+    }
 }
--- app/Models/FinancingPartner.php
+++ app/Models/FinancingPartner.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class FinancingPartner extends Model
 {
     use HasFactory;
+
+    protected $table = 'financing_partners';
+
+    protected $fillable = [
+        'user_id', 'partner_code', 'partner_name', 'registration_number',
+        'tin', 'contact_person', 'phone', 'email', 'address', 'website',
+        'logo_url', 'interest_rate', 'max_loan_term_months', 'min_loan_amount',
+        'max_loan_amount', 'is_active', 'status', 'approved_by', 'approved_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function loans()
+    {
+        return $this->hasMany(FinancingLoan::class, 'financing_partner_id');
+    }
+
+    public function disbursements()
+    {
+        return $this->hasMany(FinancingDisbursement::class, 'financing_partner_id');
+    }
+
+    public function financingRequests()
+    {
+        return $this->hasMany(PremiumFinancingRequest::class, 'premium_financing_partner_id');
+    }
 }
