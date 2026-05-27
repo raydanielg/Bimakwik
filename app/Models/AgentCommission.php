4a38e7fd2daf97b4794b3b9d43ab39a630c79f0e
raydanielg <airezra2+1@gmail.com>
2026-05-27 23:03:56 +0300

    commit

--- app/Models/AgentCommission.php
+++ app/Models/AgentCommission.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class AgentCommission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'agent_id', 'customer_policy_id', 'insurance_product_id',
+        'premium_amount', 'commission_percentage', 'commission_amount',
+        'status', 'paid_at', 'payment_transaction_id',
+    ];
+
+    protected $casts = ['paid_at' => 'datetime'];
+
+    public function agent()
+    {
+        return $this->belongsTo(Agent::class);
+    }
+
+    public function customerPolicy()
+    {
+        return $this->belongsTo(CustomerPolicy::class);
+    }
+
+    public function insuranceProduct()
+    {
+        return $this->belongsTo(InsuranceProduct::class);
+    }
 }
--- app/Models/PremiumFinancingRequest.php
+++ app/Models/PremiumFinancingRequest.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class PremiumFinancingRequest extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'customer_id', 'customer_policy_id', 'premium_amount', 'financing_amount',
+        'interest_rate', 'repayment_months', 'monthly_installment', 'status',
+        'premium_financing_partner_id', 'approved_at',
+    ];
+
+    protected $casts = ['approved_at' => 'datetime'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function customerPolicy()
+    {
+        return $this->belongsTo(CustomerPolicy::class);
+    }
+
+    public function financingPartner()
+    {
+        return $this->belongsTo(FinancingPartner::class, 'premium_financing_partner_id');
+    }
 }
