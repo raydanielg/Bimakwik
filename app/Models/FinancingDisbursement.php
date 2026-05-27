17142ba260444536c16a5cc6b4748e65814730e8
raydanielg <airezra2+1@gmail.com>
2026-05-27 20:00:15 +0300

    commit

--- app/Models/FinancingDisbursement.php
+++ app/Models/FinancingDisbursement.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class FinancingDisbursement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'financing_loan_id', 'financing_partner_id', 'disbursement_amount',
+        'disbursement_reference', 'payment_transaction_id', 'destination_type',
+        'destination_details', 'disbursed_by', 'disbursed_at', 'status',
+    ];
+
+    public function loan()
+    {
+        return $this->belongsTo(FinancingLoan::class, 'financing_loan_id');
+    }
+
+    public function financingPartner()
+    {
+        return $this->belongsTo(FinancingPartner::class, 'financing_partner_id');
+    }
 }
--- app/Models/FinancingRepaymentSchedule.php
+++ app/Models/FinancingRepaymentSchedule.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class FinancingRepaymentSchedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'financing_loan_id', 'installment_number', 'due_date', 'installment_amount',
+        'principal_portion', 'interest_portion', 'paid_amount', 'paid_at',
+        'payment_transaction_id', 'status', 'late_fee_amount',
+    ];
+
+    protected $casts = ['due_date' => 'date', 'paid_at' => 'datetime'];
+
+    public function loan()
+    {
+        return $this->belongsTo(FinancingLoan::class, 'financing_loan_id');
+    }
 }
