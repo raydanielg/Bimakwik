8daf31bce148f4fba13ca22e65a1eaba1652c8e0
raydanielg <airezra2+1@gmail.com>
2026-05-28 10:01:55 +0300

    commit

--- app/Http/Controllers/Agent/DashboardController.php
+++ app/Http/Controllers/Agent/DashboardController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('agent.dashboard');
+        return app(AgentDashboardController::class)->index($request);
     }
 }
