cb1f1ffc04e658eee0cbfbae450fd84691595d60
raydanielg <airezra2+1@gmail.com>
2026-05-28 08:30:42 +0300

    commit

--- app/Models/ServiceProvider.php
+++ app/Models/ServiceProvider.php
@@ -9,8 +9,23 @@ class ServiceProvider extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'user_id', 'service_provider_type_id', 'provider_name', 'provider_code',
+        'registration_number', 'phone', 'email', 'address', 'status',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function serviceProviderType()
     {
         return $this->belongsTo(ServiceProviderType::class, 'service_provider_type_id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(ServiceProviderPayment::class, 'service_provider_id');
     }
 }
--- app/Models/ServiceProviderPayment.php
+++ app/Models/ServiceProviderPayment.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class ServiceProviderPayment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'service_provider_id', 'claim_id', 'amount', 'payment_date',
+        'payment_reference', 'status', 'processed_by',
+    ];
+
+    protected $casts = ['payment_date' => 'date'];
+
+    public function serviceProvider()
+    {
+        return $this->belongsTo(ServiceProvider::class, 'service_provider_id');
+    }
+
+    public function claim()
+    {
+        return $this->belongsTo(Claim::class);
+    }
 }
