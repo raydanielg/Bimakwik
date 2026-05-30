d403af1fff3059dfcfa358f1c6e88d1660ae7db9
raydanielg <airezra2+1@gmail.com>
2026-05-30 14:08:09 +0300

    commit

--- app/Http/Controllers/Customer/PortalController.php
+++ app/Http/Controllers/Customer/PortalController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
