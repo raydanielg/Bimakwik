57efddfb8f117fe609deb5962cde1dd59eca667d
raydanielg <airezra2+1@gmail.com>
2026-05-30 14:01:02 +0300

    commit

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('dashboard_title', __('customer.my_insurance_dashboard'))
 
-@section('dashboard_content')
+@section('content')
 <!-- Customer Welcome Banner -->
 <div class="row mb-4">
     <div class="col-12">
