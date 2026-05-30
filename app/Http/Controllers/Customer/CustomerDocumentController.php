6fd836874b14e67db16117f7d74a82ec7c7ae98b
raydanielg <airezra2+1@gmail.com>
2026-05-30 14:04:13 +0300

    commit

--- app/Http/Controllers/Customer/CustomerDocumentController.php
+++ app/Http/Controllers/Customer/CustomerDocumentController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\CustomerPolicy;
+use App\Models\PolicyDocument;
 
 class CustomerDocumentController extends Controller
 {
-    //
+    public function index()
+    {
+        $documents = collect();
+        try {
+            $documents = PolicyDocument::whereHas('policy', function($q) {
+                $q->where('customer_id', auth()->id());
+            })->with('policy')->latest()->paginate(15);
+        } catch (\Exception $e) {}
+        return view('customer.policies.documents', compact('documents'));
+    }
 }
--- app/Http/Controllers/Customer/CustomerRenewalController.php
+++ app/Http/Controllers/Customer/CustomerRenewalController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\CustomerPolicy;
 
 class CustomerRenewalController extends Controller
 {
-    //
+    public function index()
+    {
+        $renewals = collect();
+        try {
+            $renewals = CustomerPolicy::where('customer_id', auth()->id())
+                ->where('end_date', '<=', now()->addDays(30))
+                ->with('product')
+                ->latest()
+                ->paginate(10);
+        } catch (\Exception $e) {}
+        return view('customer.policies.renewals', compact('renewals'));
+    }
 }
