86d8f52b5c0728cdd532fe0ad38a639373019c36
raydanielg <airezra2+1@gmail.com>
2026-06-01 00:10:08 +0300

    commit

--- app/Models/BrokerCommission.php
+++ app/Models/BrokerCommission.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class BrokerCommission extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'broker_id',
+        'customer_policy_id',
+        'insurance_product_id',
+        'premium_amount',
+        'commission_percentage',
+        'commission_amount',
+        'status',
+        'paid_at',
+        'payment_transaction_id',
+    ];
+
+    protected $casts = [
+        'paid_at' => 'datetime',
+        'commission_amount' => 'decimal:2',
+        'premium_amount' => 'decimal:2',
+    ];
+
+    public function broker()
+    {
+        return $this->belongsTo(Broker::class);
+    }
+
+    public function customerPolicy()
+    {
+        return $this->belongsTo(CustomerPolicy::class);
+    }
 }
